76807981d877aebe7faf226232fe33f7a5ce73a4
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-15 11:44:01 +0200

    Update login to handle 400 cases

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -4,5 +4,11 @@ export default (req, res) => {
 
     if(method !== 'POST') {
         return res.status(404).end();
+    };
+
+    if(!email || !password) {
+        return res.status(400).json({
+            error: 'Missing required params.'
+        });
     }
 }
